b32147fb3bb57093ef3f5de58b4cdf0b6a61bec5
Lukas Leindals <lukas@leindals.dk>
2023-01-10 13:38:46 +0100

    move files to Poisson

--- assignment2_parallel_computing/alloc3d.h
+++
@@ -1,9 +0,0 @@
-#ifndef __ALLOC_3D
-#define __ALLOC_3D
-
-double ***malloc_3d(int m, int n, int k);
-
-#define HAS_FREE_3D
-void free_3d(double ***array3D, int m, int n, int k);
-
-#endif /* __ALLOC_3D */
