efc4fcde1b6cbcc65c1fbd51c5a6ff61531c5382
Lukas Leindals <s183920@student.dtu.dk>
2023-01-13 13:54:53 +0100

    fix scal exp

--- assignment2_parallel_computing/Poisson3D/exp_scalability.sh
+++ assignment2_parallel_computing/Poisson3D/exp_scalability.sh
@@ -49,18 +49,19 @@ for opt_method in $opt_methods; do
 
     # run tests - WARNING the programs does not ouput the needed data yet
     # echo "Running memory scalability experiment for serial code"
-    version=1
+    versions="1 2"
     for n_threads in $( eval echo {1..$LSB_DJOB_NUMPROC..$thread_step_size} ); do
         echo "Threads = $n_threads"
         echo "Version = $version"
         for n in $Ns; do
             echo "N = $n"
-            OMP_NUM_THREADS=${n_threads} ./poisson_j $n $max_iters $tol $start_T 0 $version > $OUT_DIR/output_j_N_${n}_threads_${n_threads}_v${version}.txt
-            # OMP_NUM_THREADS=${n_threads} ./poisson_j $n $max_iters $tol $start_T 0 $version > $OUT_DIR/output_j_N_${n}_threads_${n_threads}_v${version}.txt
-            # todo: add other better implmentations
+            for v in $versions; do
+                echo "Version = $v"
+                OMP_NUM_THREADS=${n_threads} ./poisson_serial $n $max_iters $tol $start_T 0 $v > $OUT_DIR/output_serial_N_${n}_threads_${n_threads}_v${v}.txt
+            done
         done;
         version=2
-        OMP_NUM_THREADS=${n_threads} ./poisson_j $n $max_iters $tol $start_T 0 $version > $OUT_DIR/output_j_N_${n}_threads_${n_threads}_v${version}.txt
+        # OMP_NUM_THREADS=${n_threads} ./poisson_j $n $max_iters $tol $start_T 0 $version > $OUT_DIR/output_j_N_${n}_threads_${n_threads}_v${version}.txt
         OMP_NUM_THREADS=${n_threads} ./poisson_gs $n $max_iters $tol $start_T 0 $version > $OUT_DIR/output_gs_N_${n}_threads_${n_threads}_v${version}.txt
     done;echo
 done
