c4e65047f69e2624c3b7552228b251b57ab9a4e4
Gustav <s180820@student.dtu.dk>
2023-01-13 15:05:07 +0100

    still plotting away

--- assignment2_parallel_computing/Poisson3D/exp_thread_scalability.sh
+++ assignment2_parallel_computing/Poisson3D/exp_thread_scalability.sh
@@ -46,9 +46,9 @@ for n_threads in $( eval echo {1..$LSB_DJOB_NUMPROC..$thread_step_size} ); do
     echo "Threads = $n_threads"
     for n in $Ns; do
         echo "N = $n"
-        OMP_NUM_THREADS=$n_threads ./poisson_j $n $max_iters $tol $start_T 0 0 1 > $OUT_DIR/output_j_N_${n}_threads_${n_threads}_v1.txt
-        OMP_NUM_THREADS=$n_threads ./poisson_gs $n $max_iters $tol $start_T 0 0 1 > $OUT_DIR/output_gs_N_${n}_threads_${n_threads}_v1.txt
-        OMP_NUM_THREADS=$n_threads ./poisson_j $n $max_iters $tol $start_T 0 0 2 > $OUT_DIR/output_j_N_${n}_threads_${n_threads}_v2.txt
+        OMP_NUM_THREADS=$n_threads ./poisson_j $n $max_iters $tol $start_T 0 1 > $OUT_DIR/output_j_N_${n}_threads_${n_threads}_v1.txt
+        OMP_NUM_THREADS=$n_threads ./poisson_gs $n $max_iters $tol $start_T 0 2 > $OUT_DIR/output_gs_N_${n}_threads_${n_threads}_v1.txt
+        OMP_NUM_THREADS=$n_threads ./poisson_j $n $max_iters $tol $start_T 0 2 > $OUT_DIR/output_j_N_${n}_threads_${n_threads}_v2.txt
         #OMP_NUM_THREADS=$n_threads ./poisson_gs $n $max_iters $tol $start_T 0 0 2 > $OUT_DIR/output_gs_N_${n}_threads_${n_threads}_v2.txt
     done
 done
