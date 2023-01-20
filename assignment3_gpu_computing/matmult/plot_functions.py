8c7d1dd6253f61f9520ab4a2b3d8ddbac765d485
s184012 <s184012@student.dtu.dk>
2023-01-20 13:36:24 +0100

    updated io + imgs

--- assignment3_gpu_computing/matmult/plot_functions.py
+++ assignment3_gpu_computing/matmult/plot_functions.py
@@ -3,17 +3,26 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
+sns.set(rc={'figure.figsize':(9,6)})
 sns.set_style("whitegrid")
 # sns.set(rc={'figure.figsize':(11.7,8.27)})
 
 def plot_block_experiment(experiment: str):
     df = io_functions.read_experiment(experiment)
-    print(df)
-    # fig = sns.lineplot(data=df, x='block_size', y='performance', hue='version')
-    # plt.savefig('test_img.png')
+    omp_df = df.query('version=="blk_omp"')
+
+    omp_df.iloc[:, 1:-1] = omp_df.iloc[:, 1:-1].astype(float)
+    fig = sns.lineplot(data=omp_df, x='block_size', y='performance')
+
+    df_max = omp_df.query("(performance == performance.max())")[["block_size", "performance"]].reset_index(drop=True)
+    fig = sns.scatterplot(ax=fig, data=df_max, x='block_size', y='performance', color='red')
+    fig.text(x=df_max['block_size'] + 0.2, y=df_max['performance'], s=f"Optimal Blocksize: {df_max.at[0, 'block_size']}")
+    fig.set_title("blk_omp, 2048x2048 matrices")
+    print(df_max.at[0, 'block_size'])
+    plt.savefig('test_img.png')
 
 # def 
 
 if __name__ == '__main__':
 
-    plot_block_experiment("blk_size_2048_20230120_110815")
+    plot_block_experiment("blk_size_2048_20230120_130440")
