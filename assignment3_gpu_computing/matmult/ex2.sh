306bba026f08bb5788951ebd2d43a5f46932e075
Lukas Leindals <s183920@student.dtu.dk>
2023-01-20 09:58:19 +0100

    work on ex2 and lib_offload

--- assignment3_gpu_computing/matmult/ex2.sh
+++ assignment3_gpu_computing/matmult/ex2.sh
@@ -3,10 +3,12 @@
 # experiments name
 EXPNAME=question2_$(date +%Y%m%d_%H%M%S)
 OUTDIR=results/$EXPNAME/output_files
+PROFILE_DIR=results/$EXPNAME/profiles
 mkdir -p results/$EXPNAME
 mkdir -p $OUTDIR
 mkdir -p hpc_logs
 mkdir -p results/$EXPNAME/compile_logs
+mkdir -p $PROFILE_DIR
 
 # 02614 - High-Performance Computing, January 2022
 # 
@@ -24,6 +26,23 @@ mkdir -p results/$EXPNAME/compile_logs
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -W 10
 #BSUB -R "rusage[mem=2048]" 
+
+# SETTINGS
+SIZES="2048"
+BLKSIZE=1
+# TEAMS="2048 8192 16384 32768"
+# THREADS="1 2 4 8 16 32"
+TEAMS="8192 16384"
+THREADS="8 16"
+
+VERSIONS="mkn_offload mnk_offload"
+
+# driver options
+# export MATMULT_RESULTS=      # {[0]|1}       print result matrices (in Matlab format, def: 0)
+export MATMULT_COMPARE=1   # {0|[1]}       control result comparison (def: 1); enable(1)/disable(0) result checking
+export MFLOPS_MIN_T=3         # [3.0]         the minimum run-time (def: 3.0 s)
+# export MFLOPS_MAX_IT=1000        # [infinity]    max. no of iterations; set if you want to do profiling.
+
 
 
 # compile the code
@@ -47,35 +66,30 @@ echo "Jobid: ${LSB_JOBID}" >> results/$EXPNAME/setup.txt # write setup to file
 # safe compile options
 # cp compile.log results/$EXPNAME/compile_logs/compile.log
 
-# define the mkn values in the MKN variable
-export SIZES="512"
-
-# uncomment and set a reasonable BLKSIZE for the blk version
-export BLKSIZE=1
-
-# permuations
-export VERSIONS="mkn_offload mnk_offload"
-
-# driver options
-# export MATMULT_RESULTS=      # {[0]|1}       print result matrices (in Matlab format, def: 0)
-export MATMULT_COMPARE=1   # {0|[1]}       control result comparison (def: 1); enable(1)/disable(0) result checking
-export MFLOPS_MIN_T=3         # [3.0]         the minimum run-time (def: 3.0 s)
-# export MFLOPS_MAX_IT=1000        # [infinity]    max. no of iterations; set if you want to do profiling.
-
 # run the driver
 fnum=0
-for VERSION in $VERSIONS; do
-    for S in $SIZES; do
-        echo "Starting run $fnum using $VERSION with size $SIZE"
-        echo "version: $VERSION" > $OUTDIR/run_$fnum.txt
-        echo "size: $S" >> $OUTDIR/run_$fnum.txt
-        echo "num_cpu_cores: $LSB_DJOB_NUMPROC" >> $OUTDIR/run_$fnum.txt
-        echo "num_gpu_cores: $LSB_DJOB_GPUS" >> $OUTDIR/run_$fnum.txt
-        echo "result: $(./$EXECUTABLE $VERSION $S $S $S)"  >> $OUTDIR/run_$fnum.txt
-        fnum=$((fnum+1))
+for T in $TEAMS; do
+    for TH in $THREADS; do
+        make clean
+        make TEAMS=$T THREADS=$TH BLKSIZE=$BLKSIZE
+        for VERSION in $VERSIONS; do
+            for S in $SIZES; do
+                echo "Starting run $fnum using $VERSION with size $S, teams $T, threads $TH"
+                echo "version: $VERSION" > $OUTDIR/run_$fnum.txt
+                echo "size: $S" >> $OUTDIR/run_$fnum.txt
+                echo "num_cpu_cores: $LSB_DJOB_NUMPROC" >> $OUTDIR/run_$fnum.txt
+                echo "teams: $T" >> $OUTDIR/run_$fnum.txt
+                echo "threads: $TH" >> $OUTDIR/run_$fnum.txt
+                echo "result: $(./$EXECUTABLE $VERSION $S $S $S)"  >> $OUTDIR/run_$fnum.txt
+                fnum=$((fnum+1))
+            done
+        done
     done
-    sh profiler.sh $VERSION $S $S $S
 done
+
+
+sh profiler.sh mkn_offload 2048 $PROFILE_DIR/mkn_offload
+sh profiler.sh mnk_offload 2048 $PROFILE_DIR/mnk_offload
 
 
 
--- assignment3_gpu_computing/matmult/matmult.c
+++ assignment3_gpu_computing/matmult/matmult.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <cblas.h>
+#include <cublas_v2.h>
+#include <cuda.h>
+#include <cuda_runtime_api.h>
 #include <omp.h>
 #include "matmult.h"
 
-#define TRANSFER_TIMING
+// define variables if they dont exist - this should not be the case when compiling with make
+
+#define _TIMING
 
 
 #ifndef _BLOCK_SIZE
@@ -23,13 +28,17 @@
 #define _SLABS 4
 #endif
 
-// standard OpenMP versions
+// standard OpenMP versions for question 1
 void matmult_mkn_omp(int m,int n,int k,double **A,double **B,double **C){
+    C = init_C_omp(C,m,n); // initialize C as we are not interested in timing this
+    
+    #ifdef _TIMING // start timing
+    double t1, t2;
+    t1 = omp_get_wtime();
+    #endif // end timing
+
     #pragma omp parallel shared(A,B,C)
     {
-    C = init_C_omp(C,m,n);
-    // double t1, t2;
-    // t1 = omp_get_wtime();
     #pragma omp for
     for(int i=0;i<m;i++){
         for(int l=0;l<k;l++){
@@ -39,15 +48,25 @@ void matmult_mkn_omp(int m,int n,int k,double **A,double **B,double **C){
             }
         }
     }
-    // t2 = omp_get_wtime();
-    // printf("Time for matmult_mkn_omp: %lf\n", (t2-t1)*1e3);
+
+    #ifdef _TIMING // start timing
+    t2 = omp_get_wtime();
+    printf("Time: %lf\n", (t2-t1)*1e3);
+    #endif // end timing
+
     } // end parallel
 }
 
 void matmult_blk_omp(int m,int n,int k,double **A,double **B,double **C, int bs){
+    C = init_C_omp(C,m,n);
+
+    #ifdef _TIMING // start timing
+    double t1, t2;
+    t1 = omp_get_wtime();
+    #endif // end timing
+
     #pragma omp parallel shared(m,n,k,A,B,C,bs) 
     {
-    C = init_C_omp(C,m,n);
     #pragma omp for collapse(3)
     for(int i1=0;i1<m;i1+=bs){
         for(int l1=0;l1<k;l1+=bs){
@@ -64,32 +83,21 @@ void matmult_blk_omp(int m,int n,int k,double **A,double **B,double **C, int bs)
         }
     }
     }
+
+    #ifdef _TIMING // start timing
+    t2 = omp_get_wtime();
+    printf("Time: %lf\n", (t2-t1)*1e3);
+    #endif // end timing
 }
 
-// void matmult_blk(int m,int n,int k,double **A,double **B,double **C, int bs){
-//     C = init_C_omp(C,m,n);
-//     for(int i1=0;i1<m;i1+=bs){
-//         for(int l1=0;l1<k;l1+=bs){
-//             for(int j1=0; j1 < n; j1+=bs){
-//                 for(int i2=0; i2 < min(m-i1, bs); i2++){
-//                     for(int l2=0; l2 < min(k-l1, bs); l2++){
-//                         for(int j2=0; j2 < min(n-j1, bs); j2++){
-//                             C[i1+i2][j1+j2] += A[i1+i2][l1+l2]*B[l1+l2][j1+j2];
-//                         }
-//                     }
-//                 }
-//             }
-//         }
-//     }
-// }
-
-// Matrix multiplication with library function
 void matmult_lib(int m,int n,int k,double **A,double **B,double **C){
-    #pragma omp parallel shared(A,B,C)
-    {
     C = init_C_omp(C,m,n);
-    } // end parallel
      // C = A * B using cblas_dgemm see https://www.intel.com/content/www/us/en/develop/documentation/mkl-tutorial-c/top/multiplying-matrices-using-dgemm.html
+    #ifdef _TIMING // start timing
+    double t1, t2;
+    t1 = omp_get_wtime();
+    #endif // end timing
+
     cblas_dgemm(
         //Indicates that the matrices are stored in row major order, with the elements of each row of the matrix stored contiguously as shown in the figure above.
         CblasRowMajor, 
@@ -106,17 +114,22 @@ void matmult_lib(int m,int n,int k,double **A,double **B,double **C){
          // Array used to store matrix C and leading or the number of elements between successive rows (for row major storage) in memory. In the case of this exercise the leading dimension is the same as the number of columns.
         C[0], n
         );
+
+    #ifdef _TIMING // start timing
+    t2 = omp_get_wtime();
+    printf("Time: %lf\n", (t2-t1)*1e3);
+    #endif // end timing
 }
 
-// offload versions
+// OpenMP offload versions for question 2
 void matmult_mkn_offload(int m,int n,int k,double **A,double **B, double **C){
-    C = init_C(C,m,n);
+    C = init_C_omp(C,m,n);
     
     // timings
-    #ifdef TRANSFER_TIMING // TRANSER_TIMING start
+    #ifdef _TIMING // TRANSER_TIMING start
     double t1, t2;
     t1 = omp_get_wtime();
-    
+
     // transfer data to device
     #pragma omp target data map(to: A[0:m][0:k], B[0:k][0:n], m,k,n) map(tofrom: C[0:m][0:n])
     {
@@ -127,7 +140,7 @@ void matmult_mkn_offload(int m,int n,int k,double **A,double **B, double **C){
     #endif // TRANSER_TIMING end
     #pragma omp target teams distribute parallel for \
     map(to: A[0:m][0:k], B[0:k][0:n], m,k,n) map(tofrom: C[0:m][0:n]) \
-    num_teams(m) thread_limit(_THREADS) 
+    num_teams(_TEAMS) thread_limit(_THREADS) 
     for(int i=0;i<m;i++){
         for(int l=0;l<k;l++){
             for(int j=0;j<n;j++){
@@ -136,9 +149,9 @@ void matmult_mkn_offload(int m,int n,int k,double **A,double **B, double **C){
             }
         }
     }
-    #ifdef TRANSFER_TIMING // TRANSER_TIMING start
+    #ifdef _TIMING // TRANSER_TIMING start
     t2 = omp_get_wtime();
-    printf("Time without transfer: %f\n", 1e3*(t2-t1));
+    printf("Time without transfer (ms): %f\n", 1e3*(t2-t1));
     } // exit data
     t2 = omp_get_wtime();
     printf("Time with transfer: %f\n", 1e3*(t2-t1));
@@ -148,9 +161,9 @@ void matmult_mkn_offload(int m,int n,int k,double **A,double **B, double **C){
 }
 
 void matmult_mnk_offload(int m,int n,int k,double **A,double **B,double **C){
-    C = init_C(C,m,n);
+    C = init_C_omp(C,m,n);
     // timings
-    #ifdef TRANSFER_TIMING // TRANSER_TIMING start
+    #ifdef _TIMING // TRANSER_TIMING start
     double t1, t2;
     t1 = omp_get_wtime();
     
@@ -174,7 +187,7 @@ void matmult_mnk_offload(int m,int n,int k,double **A,double **B,double **C){
             }
         }
     }
-    #ifdef TRANSFER_TIMING // TRANSER_TIMING start
+    #ifdef _TIMING // TRANSER_TIMING start
     t2 = omp_get_wtime();
     printf("Time without transfer: %f\n", 1e3*(t2-t1));
     } // exit data
@@ -185,15 +198,21 @@ void matmult_mnk_offload(int m,int n,int k,double **A,double **B,double **C){
     #endif // TRANSER_TIMING end
 }
 
-// block offload versions
+// OpenMP offload versions with blocking for question 3
 void matmult_blk_offload(int m, int n, int k, double **A,double **B,double **C){
-    C = init_C(C,m,n);
+    C = init_C_omp(C,m,n);
+    // timings
+    #ifdef _TIMING // TRANSER_TIMING start
     double t1, t2;
     t1 = omp_get_wtime();
     #pragma omp target data map(to: A[:m][:k], B[:k][:n], m,k,n) map(tofrom: C[:m][:n])
     {
     t1 = omp_get_wtime();
-    #pragma omp target teams loop \
+    #else
+    #pragma omp target data map(to: A[:m][:k], B[:k][:n], m,k,n) map(tofrom: C[:m][:n])
+    {
+    #endif // TRANSER_TIMING end
+    #pragma omp target teams distribute parallel for \
     map(to: A[:m][:k], B[:k][:n], m,k,n) map(tofrom: C[:m][:n]) \
     num_teams(_TEAMS) thread_limit(_THREADS)\
     collapse(2)
@@ -222,51 +241,29 @@ void matmult_blk_offload(int m, int n, int k, double **A,double **B,double **C){
             }
         }
     }
+    #ifdef _TIMING // TRANSER_TIMING start
     t2 = omp_get_wtime();
-    printf("Time without transfer: %lf\n", t2-t1);
+    printf("Time without transfer: %lf\n", (t2-t1)*1e3);
     }
     t2 = omp_get_wtime();
-    printf("Time with transfer: %lf\n", t2-t1);
+    printf("Time with transfer: %lf\n", (t2-t1)*1e3);
+    #else
+    }
+    #endif // TRANSER_TIMING end
 }   
 
 
-// asy offload
-// #define SLABS 1
-// void matmult_asy_offload(int m,int n,int k,double **A,double **B, double **C){
-//     // printf("TEAMS: %d, THREADS: %d\n", _TEAMS, _THREADS);
-//     C = init_C(C,m,n);
-    
-//     #pragma omp parallel for
-//     for (int s = 0; s < SLABS; ++s){
-//         int length = m/SLABS; // assumed to be divisible such that we get an integer
-//         int start = s*length;
-//         // transfer data to device
-//         #pragma omp target data map(to: A[start:length][0:k], B[0:k][0:n], m,k,n) map(tofrom: C[start:length][0:n])
-//         {
-//             #pragma omp target teams distribute parallel for\
-//             num_teams(length) thread_limit(_THREADS) \
-//             map(to: A[start:length][0:k], B[0:k][0:n], m,k,n) map(tofrom: C[start:length][0:n])
-//             for(int i=start;i<start + length;i++){
-//                 for(int l=0;l<k;l++){
-//                     for(int j=0;j<n;j++){
-//                         #pragma omp atomic
-//                         C[i][j] += A[i][l]*B[l][j];
-//                     }
-//                 }
-//             } 
-//         } // exit data
-//         // #pragma omp taskwait
-//     } // finish async 
-// }
 
-// block offload versions
-
-
+// OpenMP offload versions with asynchronous data transfers for question 4
 void matmult_asy_offload(int m, int n, int k, double **A,double **B,double **C){
-    C = init_C(C,m,n);
+    C = init_C_omp(C,m,n);
 
+    // timings
+    #ifdef _TIMING // TRANSER_TIMING start
     double t1, t2;
     t1 = omp_get_wtime();
+    #endif // TRANSER_TIMING end
+
     #pragma omp target enter data map(alloc: A[0:m][0:k], B[0:k][0:n], C[0:m][0:n])
     #pragma omp target update to(B[0:k][0:n])
 
@@ -284,8 +281,6 @@ void matmult_asy_offload(int m, int n, int k, double **A,double **B,double **C){
         collapse(2)
         for(int i1=start; i1<start+length;i1+=_BLOCK_SIZE){
             for(int j=0;j<n;j++){
-                // int i2, l;
-
                 if (_BLOCK_SIZE -1 < (m-i1)){
                     double temp_sum[_BLOCK_SIZE] = {0};
                     for(int l=0;l<k;l++){   
@@ -293,25 +288,20 @@ void matmult_asy_offload(int m, int n, int k, double **A,double **B,double **C){
                             temp_sum[i2] += A[i1+i2][l] * B[l][j];
                         }
                     }
-                    // printf("p1 i1: %d, j: %d, i2: %d, l: %d, k: %d\n", i1, j, i2, l, k);
                     for (int i=0; i < _BLOCK_SIZE; i++){
                         C[i + i1][j] = temp_sum[i];
                     }
-                    // printf("p2 i1: %d, j: %d, i2: %d, l: %d, k: %d\n", i1, j, i2, l, k);
-
                 }
                 else { 
                     for(int i2=0; i2 < (m-i1); i2++){
                         double sum= 0;
                         for(int l=0;l<k;l++){   
-                            // C[i1+i2][j] += A[i1+i2][l]*B[l][j];
                             sum += A[i2+i1][l]*B[l][j];
                         }
                         C[i1+i2][j] = sum;
                     }
                 }
             }
-            // printf("i1: %d\n", i1);
         } // end block
         #pragma omp target update from(C[start:length][:n]) depend(in:C) nowait
         // printf("s: %d, start: %d, length: %d\n", s, start, length);
@@ -319,10 +309,239 @@ void matmult_asy_offload(int m, int n, int k, double **A,double **B,double **C){
     #pragma omp taskwait
     #pragma omp target exit data map(delete: A[0:m][0:k], B[0:k][0:n], C[0:m][0:n])
 
+    #ifdef _TIMING // TRANSER_TIMING start
     t2 = omp_get_wtime();
     printf("Time with transfer: %f\n", 1e3*(t2-t1));
+    #endif // TRANSER_TIMING end
 }   
 
+
+
+// void matmult_lib_offload(int m, int n, int k, double **A, double **B, double **C) {
+//     cublasHandle_t handle;
+//     cublasCreate(&handle);
+
+//     double *d_A, *d_B, *d_C;
+//     cudaMalloc((void **)&d_A, m * k * sizeof(double));
+//     cudaMalloc((void **)&d_B, k * n * sizeof(double));
+//     cudaMalloc((void **)&d_C, m * n * sizeof(double));
+
+//     cudaMemcpy(d_A, *A, m * k * sizeof(double), cudaMemcpyHostToDevice);
+//     cudaMemcpy(d_B, *B, k * n * sizeof(double), cudaMemcpyHostToDevice);
+
+//     double alpha = 1.0;
+//     double beta = 0.0;
+
+//     cublasDgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, m, n, k, &alpha, d_A, m, d_B, k, &beta, d_C, m);
+
+//     cudaMemcpy(*C, d_C, m * n * sizeof(double), cudaMemcpyDeviceToHost);
+
+//     cudaFree(d_A);
+//     cudaFree(d_B);
+//     cudaFree(d_C);
+//     cublasDestroy(handle);
+//     double **Ct = malloc_2d(m, n);
+//     for (int i = 0; i < m; i++) {
+//         for (int j = 0; j < n; j++) {
+//             Ct[i][j] = C[i][j];
+//         }
+//     }
+
+//     for (int i = 0; i < m; i++) {
+//         for (int j = 0; j < n; j++) {
+//             C[i][j] = C[j][i];
+//         }
+//     }
+// }
+
+
+// void matmult_lib_offload(int m,int n,int k,double **A,double **B,double **C){
+//     C = init_C_omp(C,m,n);
+// }
+
+// OpenMP library version with offload for question 5
+// https://github.com/colleeneb/openmp_offload_and_blas/blob/master/cublas/c/Makefile
+// void matmult_lib_offload(int m,int n,int k,double **A,double **B,double **C){
+//     C = init_C_omp(C,m,n);
+
+//     int teams = 0;
+//     int threads = 0;
+
+//     double *A_ptr = A[0];
+//     double *B_ptr = B[0];
+//     double *C_ptr = C[0];
+
+
+//     // Initialize CUBLAS
+//     cublasHandle_t handle;
+//     cublasCreate(&handle);
+
+//     // 
+    
+    
+
+//     // parameters
+//     double alpha = 1.0;
+//     double beta = 0.0;
+
+//     #pragma omp target enter data map(to: A_ptr[0:m*k], B_ptr[0:k*n], C_ptr[0:m*n])
+
+//     #pragma target data use_device_ptr(A_ptr, B_ptr, C_ptr)
+//     {
+//     int cublas_error = cublasDgemm(
+//         handle, // handle to the CUBLAS library context
+//         CUBLAS_OP_N, CUBLAS_OP_N,  // _N means no transpose, _T means transpose and _C means conjugate transpose for the input matrices A and B
+//         m, n, k, // size of matrices
+//         &alpha, // pointer to a scalar that is multiplied by the result of the matrix multiplication.
+//         A_ptr, m, // matrix A (pointer to the memory location of the first matrix A. It should be of size m*k) and the leading dimension (nrows) of A
+//         B_ptr, k, // matrix B (pointer to the memory location of the first matrix B. It should be of size k*n) and the leading dimension (nrows) of B
+//         &beta, // pointer to a scalar that is multiplied by the matrix C
+//         C_ptr, m // matrix C (pointer to the memory location of the first matrix C. It should be of size m*k) and the leading dimension (nrows) of C
+//     ); 
+
+//     if( cublas_error != CUBLAS_STATUS_SUCCESS ){
+//        printf( "failed %d %f.\n", cublas_error, C_ptr[0] );
+//        exit(1);
+//     }
+//     }
+
+//     cudaDeviceSynchronize(); 
+
+//     // Destroy the handle
+//     cublasDestroy(handle);
+
+//     #pragma omp target exit data map(from: C_ptr[0:m*n])
+// }
+
+
+
+
+
+void matmult_lib_offload(int m, int n, int k, double **A, double **B, double **C) {
+
+    #ifdef _TIMING // start timing
+    double t1, t2;
+    t1 = omp_get_wtime();
+    #endif // end timing
+
+
+    cublasHandle_t handle;
+    cublasCreate(&handle);
+
+    double *d_A, *d_B, *d_C;
+    cudaMalloc((void **)&d_A, m * k * sizeof(double));
+    cudaMalloc((void **)&d_B, k * n * sizeof(double));
+    cudaMalloc((void **)&d_C, m * n * sizeof(double));
+    // size_t pitch_C;
+    // cudaMallocPitch((void**)&d_C, &pitch_C, n*sizeof(double), m);
+
+    cudaMemcpy(d_A, *A, m * k * sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, *B, k * n * sizeof(double), cudaMemcpyHostToDevice);
+
+    double alpha = 1.0;
+    double beta = 0.0;
+
+    cublasDgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, m, n, k, &alpha, d_A, m, d_B, k, &beta, d_C, m);
+
+    // double alpha_transpose = 1.0;
+    // double beta_transpose = 0.0;
+    // cublasDgeam(handle, CUBLAS_OP_T, CUBLAS_OP_N, n, m, &alpha_transpose, d_C, pitch_C, &beta_transpose, d_C, m, d_C, pitch_C);
+    cudaMemcpy(*C, d_C, m * n * sizeof(double), cudaMemcpyDeviceToDevice);
+
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+    cublasDestroy(handle);
+
+    #ifdef _TIMING // start timing
+    t2 = omp_get_wtime();
+    printf("Time: %lf\n", (t2-t1)*1e3);
+    #endif // end timing
+}
+
+
+
+
+
+// #define cublas_assert(expr, fmt, ...)					\
+//     do {								\
+// 	if (!(expr)) {							\
+// 	    fprintf(stderr, "%s: line %d: in %s(): " fmt, __FILE__, __LINE__, __func__, ## __VA_ARGS__); \
+// 	    fprintf(stderr, "\n");					\
+// 	    exit(EXIT_FAILURE);						\
+// 	}								\
+//     } while(0)
+
+// OpenMP library version with offload for question 5
+// void matmult_lib_offload(int m,int n,int k,double **A,double **B,double **C){
+    // double *dA, *dB, *dC, *dCt;
+    // int lda, ldb, ldc;
+    // double alpha = 1.0, beta = 0.0;
+    // cublasHandle_t handle;
+    // cudaError_t err;
+    // cublasStatus_t stat;
+    // // int i, j, k;
+
+    // // turn A, B and C into 1D arrays
+    // double *hA = malloc(m*k*sizeof(double));
+    // double *hB = malloc(k*n*sizeof(double));
+    // double *hC = malloc(m*n*sizeof(double));
+    // for (int i = 0; i < m; i++) {
+    //     for (int j = 0; j < k; j++) {
+    //         hA[i*k + j] = A[i][j];
+    //     }
+    // }
+    // for (int i = 0; i < k; i++) {
+    //     for (int j = 0; j < n; j++) {
+    //         hB[i*n + j] = B[i][j];
+    //     }
+    // }
+    // for (int i = 0; i < m; i++) {
+    //     for (int j = 0; j < n; j++) {
+    //         hC[i*n + j] = C[i][j];
+    //     }
+    // }
+
+
+    // // lda = ldb = ldc = size;
+    // lda = m;
+    // ldb = k;
+    // ldc = m;
+    
+    // stat = cublasCreate(&handle);
+    // cublas_assert(stat == CUBLAS_STATUS_SUCCESS, "cublasCreate");
+    
+    // cudaMalloc((void**)&dA,  m*k*sizeof(*dA));
+    // cudaMalloc((void**)&dB,  k*n*sizeof(*dB));
+    // cudaMalloc((void**)&dC,  m*n*sizeof(*dC));
+    // cudaMalloc((void**)&dCt, n*m*sizeof(*dCt));
+    
+    // stat = cublasSetMatrix(m, k, sizeof(*hA), hA, lda, dA, m);
+    // cublas_assert(stat == CUBLAS_STATUS_SUCCESS, "cublasSetMatrix hA");
+    // stat = cublasSetMatrix(k, n, sizeof(*hB), hB, ldb, dB, k);
+    // cublas_assert(stat == CUBLAS_STATUS_SUCCESS, "cublasSetMatrix hB");
+    // stat = cublasSetMatrix(m, n, sizeof(*hC), hC, ldc, dC, m);
+    // cublas_assert(stat == CUBLAS_STATUS_SUCCESS, "cublasSetMatrix hC");
+
+    // // note: cublas uses column major(fortran order) matrices
+    // // http://docs.nvidia.com/cuda/cublas/index.html
+    // // so trancepose row major matrices
+    // stat = cublasDgemm(handle, CUBLAS_OP_T, CUBLAS_OP_T, m, n, k, &alpha, dA, lda, dB, ldb, &beta, dC, ldc);
+    // cublas_assert(stat == CUBLAS_STATUS_SUCCESS, "cublasDgemm");
+    // // tracepose C since the matrix is column major
+    // stat = cublasDgeam(handle, CUBLAS_OP_T, CUBLAS_OP_N, m, n, &alpha, dC, ldc, &beta, dB, ldb, dCt, ldc);
+    // cublas_assert(stat == CUBLAS_STATUS_SUCCESS, "cublasDgeam");
+    
+    // stat = cublasGetMatrix(m, n, sizeof(*hC), dCt, m, hC, ldc);
+    // cublas_assert(stat == CUBLAS_STATUS_SUCCESS, "cublasGetMatrix");
+    // cudaFree(dA);
+    // cudaFree(dB);
+    // cudaFree(dC);
+    // cudaFree(dCt);
+    
+    // stat = cublasDestroy(handle);
+    // cublas_assert(stat == CUBLAS_STATUS_SUCCESS, "cublasDestroy");
+// }
 
 
 // define helper functions
@@ -340,18 +559,8 @@ double **init_C(double **C, int m, int n){
     return C;
 }
 
-double **init_C_d(double **C, int m, int n){
-    #pragma omp target teams distribute parallel for collapse(2)
-    for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            C[i][j] = 0;
-        }
-    }
-    return C;
-}
-
 double **init_C_omp(double **C, int m, int n){
-    #pragma omp for
+    #pragma omp parallel for
     for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
             C[i][j] = 0;
